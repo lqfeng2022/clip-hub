85fa2b73205a5b8a76eb8d53bf1b86eaaf92d6e1
lqfeng2022 <simonlqf@hotmail.com>
2025-05-18 12:05:22 +0800

    create custom hook

--- src/components/ClipGrid.tsx
+++ src/components/ClipGrid.tsx
@@ -1,27 +1,9 @@
-import { useEffect, useState } from 'react';
-import apiClient from '../services/api-client';
 import { Text } from '@chakra-ui/react';
+import useClips from '../hooks/useClips'
 
-interface Clip {
-  id: number;
-  title: string;
-}
-
-interface FetchclipsResponse {
-  count: number;
-  results: Clip[];
-}
 
 const ClipGrid = () => {
-  const [clips, setclips] = useState<Clip[]>([]);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    apiClient
-      .get<FetchclipsResponse>('/videos')
-      .then((res) => setclips(res.data.results))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { clips, error } = useClips();
 
   return (
     <>
---
+++ src/hooks/useClips.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import apiClient from '../services/api-client';
+import { CanceledError } from 'axios';
+
+interface Clip {
+  id: number;
+  title: string;
+}
+
+interface FetchclipsResponse {
+  count: number;
+  results: Clip[];
+}
+
+const ClipGrid = () => {
+  const [clips, setclips] = useState<Clip[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    // Handle cancellations
+    const controller = new AbortController(); // create a controller obj
+
+    apiClient
+      .get<FetchclipsResponse>('/videos', 
+        {signal: controller.signal}) // pass a signal argment
+      .then((res) => setclips(res.data.results))
+      .catch((err) => {
+        // error handler to skip the 'canceled' message
+        if (err instanceof CanceledError) return;
+        setError(err.message)});
+    // return a cleaner f 
+    return () => controller.abort();
+  }, []); // []: dependencies
+
+  return {clips, error}
+};
+
+export default ClipGrid;
