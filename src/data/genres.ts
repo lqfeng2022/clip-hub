7f5d25097c75fb7140c06677c858c28fc2846a46
lqfeng2022 <simonlqf@hotmail.com>
2025-07-26 17:25:01 +0800

    update genres data image

--- src/data/genres.ts
+++ src/data/genres.ts
@@ -1,5 +1,5 @@
 export default {
-  "count": 12,
+  "count": 11,
   "next": null,
   "previous": null,
   "results": [
@@ -7,77 +7,77 @@ export default {
       "id": 1,
       "title": "TV Series",
       "title_ch": "电视剧",
-      "image": "https://ik.imagekit.io/miqwyhcir/clip-hub/tv.jpg?updatedAt=1747711826262",
+      "image": "http://localhost:8000/media/pocket/images/clip-genres/tv.jpg",
       "videos_count": 7
     },
     {
       "id": 2,
       "title": "Movie",
       "title_ch": "电影",
-      "image": "https://ik.imagekit.io/miqwyhcir/clip-hub/movie.jpg?updatedAt=1747712175830",
+      "image": "http://localhost:8000/media/pocket/images/clip-genres/movie.jpg",
       "videos_count": 2
     },
     {
       "id": 3,
       "title": "Animation",
       "title_ch": "动画",
-      "image": "https://ik.imagekit.io/miqwyhcir/clip-hub/animation.jpg?updatedAt=1747712176060",
+      "image": "http://localhost:8000/media/pocket/images/clip-genres/animation.jpg",
       "videos_count": 2
     },
     {
       "id": 4,
       "title": "Documentary",
       "title_ch": "纪录片",
-      "image": "https://ik.imagekit.io/miqwyhcir/clip-hub/documentary.jpg?updatedAt=1747713698060",
+      "image": "http://localhost:8000/media/pocket/images/clip-genres/documentary.jpg",
       "videos_count": 2
     },
     {
       "id": 5,
       "title": "Talk Show",
       "title_ch": "脱口秀",
-      "image": "https://ik.imagekit.io/miqwyhcir/clip-hub/talkshow.webp?updatedAt=1747713698195",
+      "image": "http://localhost:8000/media/pocket/images/clip-genres/talkshow.webp",
       "videos_count": 2
     },
     {
       "id": 6,
       "title": "News",
       "title_ch": "新闻",
-      "image": "https://ik.imagekit.io/miqwyhcir/clip-hub/news.jpg?updatedAt=1747713698522",
+      "image": "http://localhost:8000/media/pocket/images/clip-genres/news.jpg",
       "videos_count": 1
     },
     {
       "id": 8,
       "title": "Short Films",
       "title_ch": "短剧",
-      "image": "https://ik.imagekit.io/miqwyhcir/clip-hub/short_film.jpg?updatedAt=1747713698254",
-      "videos_count": 4
+      "image": "http://localhost:8000/media/pocket/images/clip-genres/short-film.jpg",
+      "videos_count": 2
     },
     {
       "id": 9,
       "title": "Podcast",
       "title_ch": "播客",
-      "image": "https://ik.imagekit.io/miqwyhcir/clip-hub/podcast.webp?updatedAt=1747713698477",
+      "image": "http://localhost:8000/media/pocket/images/clip-genres/podcast.webp",
       "videos_count": 1
     },
     {
       "id": 10,
       "title": "Interview",
       "title_ch": "采访",
-      "image": "https://ik.imagekit.io/miqwyhcir/clip-genres/interview.webp?updatedAt=1750396318276",
+      "image": "http://localhost:8000/media/pocket/images/clip-genres/interview.webp",
       "videos_count": 1
     },
     {
       "id": 11,
       "title": "Game",
       "title_ch": "游戏",
-      "image": "https://ik.imagekit.io/miqwyhcir/clip-hub/game.jpeg?updatedAt=1747713698434",
+      "image": "http://localhost:8000/media/pocket/images/clip-genres/game.jpeg",
       "videos_count": 1
     },
     {
       "id": 12,
       "title": "Sport",
       "title_ch": "运动",
-      "image": "https://ik.imagekit.io/miqwyhcir/clip-hub/sport.jpg?updatedAt=1747713698444",
+      "image": "http://localhost:8000/media/pocket/images/clip-genres/sport.jpg",
       "videos_count": 1
     }
   ]
