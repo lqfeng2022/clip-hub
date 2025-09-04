61bc595892a566fe1b553805e94e0bc8620e2820
lqfeng2022 <simonlqf@hotmail.com>
2025-09-04 19:12:54 +0800

    ClipDetailPage layout adjustment

--- src/components/clip/ClipSubtitles.tsx
+++ src/components/clip/ClipSubtitles.tsx
@@ -1,7 +1,6 @@
 import { Subtitle } from '@/entities/Subtitle'
 import { parseTimeline, sortSubtitlesByTimeline } from '@/helperfunction'
-import useLanguageStore from '@/languageStore'
-import { Box, Heading, SimpleGrid } from '@chakra-ui/react'
+import { SimpleGrid } from '@chakra-ui/react'
 import { RefObject } from 'react'
 import ClipSubtitleTimeline from './ClipSubtitleTimeline'
 
@@ -10,9 +9,6 @@ interface Props {
   videoRef: RefObject<HTMLVideoElement>,
 }
 const ClipSubtitles = ({ subtitles, videoRef }: Props) => {
-  const lang = useLanguageStore(s => s.language)
-  const heading = lang === 'en' ? 'Subtitles' : '视频字幕'
-
   const sortedData = subtitles 
     ? sortSubtitlesByTimeline(subtitles) : []
 
@@ -25,12 +21,6 @@ const ClipSubtitles = ({ subtitles, videoRef }: Props) => {
   }
 
   return (
-    <>
-      <Box as='span' flex='1' textAlign='left'>
-        <Heading size='md' color='gray'>
-          {heading}
-        </Heading>
-      </Box>
       <SimpleGrid pt='15px' spacing={5}>
         {sortedData?.map((e) => (
           <ClipSubtitleTimeline
@@ -40,7 +30,6 @@ const ClipSubtitles = ({ subtitles, videoRef }: Props) => {
           />
         ))}
       </SimpleGrid>
-    </>
   )
 }
 
--- src/pages/ClipDetailPage.tsx
+++ src/pages/ClipDetailPage.tsx
@@ -19,32 +19,44 @@ const ClipDetailPage = () => {
     ? clip?.title_ch : clip?.title
   const about_content = lang === 'ch' && clip?.description_ch 
     ? clip.description_ch : clip?.description
+  const heading = lang === 'en' ? 'Subtitles' : '视频字幕'
 
   if (isLoading) return <Spinner/>
   if (error || !clip) throw error 
   return (
     <Grid
       templateAreas={{ base: `'main'`, lg: `'left right'` }}
-      templateColumns={{ base: '1fr', lg: '1fr 2fr' }}
+      templateColumns={{ base: '1fr', lg: '3fr 2fr' }}
       gap={5}
       px={2}
       pb='50px'
     >
+      {/* 2. video and subtitles */}
       <GridItem order={{ base: 2, lg: 1 }}>
-        <ClipSubtitles subtitles={clip.subtitles} videoRef={videoRef}/>
-      </GridItem>
-      <GridItem order={{ base: 1, lg: 2 }}>
         <ClipMovie video={clip} videoRef={videoRef}/>
+          <Box mt={2}>
+            <Box as='span' flex='1' textAlign='left'>
+              <Heading size='md' py={1.5} color='gray'>
+                {heading}
+              </Heading>
+            </Box>
+            <Box flex='1' overflowY='auto' maxH='50vh'>
+              <ClipSubtitles subtitles={clip.subtitles} videoRef={videoRef}/>
+            </Box>
+          </Box>
+      </GridItem>
+      {/* 1. video info */}
+      <GridItem order={{ base: 1, lg: 2 }}>
+        {/* 1.1 video header */}
         <Heading py={3}>{header}</Heading>
-        <Box py={2}>
-          <Heading size='md' pb={1} color='gray.500'>
+        {/* 1.2 video details */}
+        <Box>
+          <Heading size='md' color='gray.500'>
             {about}
           </Heading>
-          <Text>
-            {about_content}
-          </Text>
+          <Text py={2}>{about_content}</Text>
+          <ClipAttributes clip={clip}/>
         </Box>
-        <ClipAttributes clip={clip}/>
       </GridItem>
     </Grid>
   )
