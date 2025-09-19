9368496fedb30af94b329c0db108c450f607a2c3
lqfeng2022 <simonlqf@hotmail.com>
2025-09-19 16:05:40 +0800

    ProfileBack text edit

--- src/components/profile/ProfileBack.tsx
+++ src/components/profile/ProfileBack.tsx
@@ -7,11 +7,11 @@ const ProfileBack = () => {
       borderRadius={5}
       bgGradient='linear(to-r, gray.200, gray.400, gray.600)'
     >
-      <Text p={5}>
+      <Text p='10px 20px'>
         Hey there! I’m <strong>Simon Lee</strong>, the creator of <i>ClipWords</i>. 
         I’m creating <strong>a fun AI agent</strong> on top of our English Expression database. 
-        Think of it as <strong>a friendly companion</strong> — witty, supportive, and always up 
-        for a conversation — here to help you learn.
+        Think of it as <strong>a friendly English-Speaking companion</strong> — witty, supportive, and always up 
+        for a conversation...
       </Text>
     </Box>
   )
